5218e6482117ff84ead0e7b9926a4488da86af3e
Gavin Chohan <gavin.chohan@sky.uk>
2023-10-12 15:59:55 +0100

    Updated logic and tests for Fizz, Buzz, and FizzBuzz scenarios.
    Created unit tests for expected results, and expanded logic for decider (logic still doesn't function as it should yet).

--- fizzbuzz/logic/FizzBuzzDecider.swift
+++ fizzbuzz/logic/FizzBuzzDecider.swift
@@ -8,8 +8,33 @@
 import Foundation
 
 struct FizzBuzzDecider {
-    func fizzes (numbers: [Int]) -> [String] {
+    func fizzes(numbers: [Int]) -> [String] {
         return numbers.map { number in
+            
+            // Fizz
+            let remainderWhenDividedByThree = number % 3
+            print("\(remainderWhenDividedByThree)")
+            
+            // Buzz
+            let remainderWhenDividedByFive = number % 5
+            print("\(remainderWhenDividedByFive)")
+            
+            // FizzBuzz
+            let remainderWhenDividedByThreeAndFive = number % 3; number % 5
+            print ("\(remainderWhenDividedByThreeAndFive)")
+            
+            if remainderWhenDividedByThree == 0 {
+                return "Fizz"
+            }
+            
+            if remainderWhenDividedByFive == 0 {
+                return "Buzz"
+            }
+            
+            if remainderWhenDividedByThreeAndFive == 0 {
+                return "FizzBuzz"
+            }
+                
             return "\(number)"
         }
     }
--- fizzbuzzTests/fizzbuzzTests.swift
+++ fizzbuzzTests/fizzbuzzTests.swift
@@ -5,12 +5,10 @@
 //  Created by Gavin Chohan on 12/10/2023.
 //
 
-import XCTest
 @testable import fizzbuzz
+import XCTest
 
 final class fizzbuzzTests: XCTestCase {
-
-
     func testFirstFifteenValues() throws {
         /**
          Fizz Buzz rules
@@ -21,12 +19,9 @@ final class fizzbuzzTests: XCTestCase {
          if divisble by neither, return the provided number
          */
         let fizzBuzzDecider = FizzBuzzDecider()
-        let input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-        let expectedOutput = ["1","2","Fizz","4","Buzz","Fizz", "7", "8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+        let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        let expectedOutput = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
         let actualOutput = fizzBuzzDecider.fizzes(numbers: input)
         XCTAssertEqual(expectedOutput, actualOutput, "Incorrect values")
     }
-
-    
-
 }
