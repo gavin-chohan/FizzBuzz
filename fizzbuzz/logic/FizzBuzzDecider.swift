11edc2bdd65ed19119f398913fd9d773c8ab906d
Gavin Chohan <gavin.chohan@sky.uk>
2023-10-13 15:00:40 +0100

    Overloaded the fizzes function to accept one integer.

--- fizzbuzz/logic/FizzBuzzDecider.swift
+++ fizzbuzz/logic/FizzBuzzDecider.swift
@@ -9,23 +9,35 @@ import Foundation
 
 struct FizzBuzzDecider {
     func fizzes(numbers: [Int]) -> [String] {
-        fizzes1(numbers: numbers)
-    }
-    
-    func fizzes1(numbers: [Int]) -> [String] {
         return numbers.map { number in
+            fizzes(number: number)
+        }
+    }
 
-            // FizzBuzz
+    func fizzes(number: Int) -> String {
+        let isDivisibleByThree = number % 3 == 0
+        let isDivisibleByFive = number % 5 == 0
+        if isDivisibleByThree, isDivisibleByFive {
+            return "FizzBuzz"
+        }
+        if isDivisibleByThree {
+            return "Fizz"
+        }
+        if isDivisibleByFive {
+            return "Buzz"
+        }
+        return "\(number)"
+    }
+
+    private func fizzesAttempt1(numbers: [Int]) -> [String] {
+        return numbers.map { number in
             if number % 3 == 0, number % 5 == 0 {
                 return "FizzBuzz"
             }
-
-            // Fizz
             if number % 3 == 0 {
                 return "Fizz"
             }
-            
-            // Buzz
+
             if number % 5 == 0 {
                 return "Buzz"
             }
@@ -34,44 +46,18 @@ struct FizzBuzzDecider {
         }
     }
 
-    func fizzes2(numbers: [Int]) -> [String] {
+    private func fizzesAttempt2(numbers: [Int]) -> [String] {
         return numbers.map { number in
 
-            // Fizz
             let remainderWhenDividedByThree = number % 3
-
-            // Buzz
             let remainderWhenDividedByFive = number % 5
-
-            // FizzBuzz
             if remainderWhenDividedByThree == 0, remainderWhenDividedByFive == 0 {
                 return "FizzBuzz"
             }
-
             if remainderWhenDividedByThree == 0 {
                 return "Fizz"
             }
-
             if remainderWhenDividedByFive == 0 {
-                return "Buzz"
-            }
-
-            
-            return "\(number)"
-        }
-    }
-    
-    func fizzes3(numbers: [Int]) -> [String] {
-        return numbers.map { number in
-            let isDivisibleByThree = number % 3 == 0
-            let isDivisibleByFive = number % 5 == 0
-            if isDivisibleByThree, isDivisibleByFive {
-                return "FizzBuzz"
-            }
-            if isDivisibleByThree {
-                return "Fizz"
-            }
-            if isDivisibleByFive {
                 return "Buzz"
             }
             return "\(number)"
--- fizzbuzzTests/fizzbuzzTests.swift
+++ fizzbuzzTests/fizzbuzzTests.swift
@@ -8,16 +8,29 @@
 @testable import fizzbuzz
 import XCTest
 
+/**
+ Fizz Buzz rules
+ Provide a number
+ if divisible by 3, return Fizz
+ if divisible by 5, return Buzz
+ if divisible by 3 and 5, return FizzBuzz
+ if divisble by neither, return the provided number
+ */
+
 final class fizzbuzzTests: XCTestCase {
     func testFirstFifteenValues() throws {
-        /**
-         Fizz Buzz rules
-         Provide a number
-         if divisible by 3, return Fizz
-         if divisible by 5, return Buzz
-         if divisible by 3 and 5, return FizzBuzz
-         if divisble by neither, return the provided number
-         */
+        let fizzBuzzDecider = FizzBuzzDecider()
+        let inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        let expectedOutputs = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
+
+        for (index, number) in inputs.enumerated() {
+            let actualOutput = fizzBuzzDecider.fizzes(number: number)
+            let expectedOutput = expectedOutputs[index]
+            XCTAssertEqual(expectedOutput, actualOutput, "Incorrect value")
+        }
+    }
+
+    func testFirstFifteenValuesOfAnArray() throws {
         let fizzBuzzDecider = FizzBuzzDecider()
         let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
         let expectedOutput = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
