0a6a98ed2d19e46b048e1e1f4ccb986e944c93f7
Gavin Chohan <gavin.chohan@sky.uk>
2023-10-13 15:18:15 +0100

    Updated tests.

--- fizzbuzzTests/fizzbuzzTests.swift
+++ fizzbuzzTests/fizzbuzzTests.swift
@@ -18,11 +18,11 @@ import XCTest
  */
 
 final class fizzbuzzTests: XCTestCase {
+    let inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    let expectedOutputs = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
+    
     func testFirstFifteenValues() throws {
         let fizzBuzzDecider = FizzBuzzDecider()
-        let inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-        let expectedOutputs = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
-
         for (index, number) in inputs.enumerated() {
             let actualOutput = fizzBuzzDecider.fizzes(number: number)
             let expectedOutput = expectedOutputs[index]
@@ -32,9 +32,7 @@ final class fizzbuzzTests: XCTestCase {
 
     func testFirstFifteenValuesOfAnArray() throws {
         let fizzBuzzDecider = FizzBuzzDecider()
-        let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-        let expectedOutput = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]
-        let actualOutput = fizzBuzzDecider.fizzes(numbers: input)
-        XCTAssertEqual(expectedOutput, actualOutput, "Incorrect values")
+        let actualOutput = fizzBuzzDecider.fizzes(numbers: inputs)
+        XCTAssertEqual(expectedOutputs, actualOutput, "Incorrect values")
     }
 }
